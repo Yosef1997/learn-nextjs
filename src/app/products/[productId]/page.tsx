13260a33bc08c1f7fbdaf1419050e728caf5b4b6
JanBu <yosefsitumorang97@gmail.com>
2024-04-09 16:52:24 +0700

    feat(layout): implement routing metada

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,24 @@
-export default function PageDetails({
+import { Metadata } from 'next'
+
+type Props = {
+  params: {
+    productId: String
+  }
+}
+
+export const generateMetadata = async ({
   params,
-}: {
-  params: { productId: string }
-}) {
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`)
+    }, 100)
+  })
+  return {
+    title: `Product ${params.productId}`,
+  }
+}
+
+export default function ProductDetails({ params }: Props) {
   return <h1>Detail about product {params.productId}</h1>
 }
